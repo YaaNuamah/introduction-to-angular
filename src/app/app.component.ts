3f03e7aef83404193f8103c39ca411ec79b8b3a0
YaaNuamah <yaa_fordjour@hotmail.com>
2020-04-23 14:56:31 +0000

    Data Binding Techniques

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'introduction-to-angular';
+  title = 'Data Binding Example using Two Way Binding';
+    imgUrl = "https://peerbits-wpengine.netdna-ssl.com/wp-content/uploads/2019/04/whats-new-in-angular-min.jpg"
+    
+    onSave($event) {
+      console.log("Save button is clicked!", $event);
+   }
+
+   fullName: String = "Hello Frederick";
+
 }
